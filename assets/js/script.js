693e6c05c031be5607f532711f04f575346e0c03
Nicholas Brown <brown.nicholasj@gmail.com>
2021-04-03 06:56:04 -0500

    everything working except leaderboard storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,4 @@
-this;
-// base document create elements
-var body = document.body;
-
+// variable storage
 var timerElement = document.querySelector(`#timer`);
 var timerValue = 60;
 var wrongPenalty = -5;
@@ -12,6 +9,11 @@ var buttonA = document.querySelector(`#a`);
 var buttonB = document.querySelector(`#b`);
 var buttonC = document.querySelector(`#c`);
 var buttonD = document.querySelector(`#d`);
+var buttonReset = document.querySelector(`#reset`);
+var leaderboard = document.querySelector(`#leaderboard`);
+var records = document.querySelector(`#records`);
+var resetLeader = document.querySelector(`#resetLeader`);
+var saveButton = document.querySelector(`#save`);
 
 // // Add text to elements
 timerElement.textContent = `Timer: ${timerValue}`;
@@ -64,14 +66,18 @@ function countdown() {
 	}, 1000);
 }
 
+// display message at end of quiz IF you run out of time
 function displayMessage() {
 	qHeader.textContent = `OUT OF TIME`;
 
 	for (var i = 0; i < buttonContainer.children.length; i++) {
 		buttonContainer.children[i].setAttribute(`style`, `display: none;`);
 	}
+
+	buttonReset.setAttribute(`style`, `display: block;`);
 }
 
+// Penalty function to take away time for wrong answer
 function penalty() {
 	if (timerValue >= 6) {
 		timerValue += wrongPenalty;
@@ -129,11 +135,16 @@ buttonContainer.addEventListener('click', function (event) {
 			askQuestion();
 		}
 	}
+	if (butID === `reset`) {
+		resetQuiz();
+	}
 });
 
+// Set quiz variables for question num and current answer storage
 var questionNumber = 0;
 var currentAnswer = ``;
 
+// function to generate each question
 function askQuestion() {
 	var problem = [
 		{
@@ -232,10 +243,108 @@ function askQuestion() {
 	questionNumber++;
 
 	if (questionNumber === 10) {
-		qHeader.textContent = `Complete with a time of ${timerValue}`;
+		qHeader.textContent = `Completed with a time of ${timerValue}`;
 
 		for (var i = 0; i < buttonContainer.children.length; i++) {
 			buttonContainer.children[i].setAttribute(`style`, `display: none;`);
 		}
+
+		leaderboard.setAttribute(`style`, `display:block;`);
+		records.setAttribute(`style`, `display:block`);
+		saveButton.setAttribute(`style`, `display:block`);
+		buttonReset.setAttribute(`style`, `display:block;`);
+		resetLeader.setAttribute(`style`, `display:block;`);
 	}
 }
+
+// function to reset the quiz by reload()
+function resetQuiz() {
+	questionNumber = 0;
+	currentAnswer = ``;
+}
+
+// Leaderboard construction
+var initials = document.querySelector(`#initials`);
+
+function updateLeaders() {
+	var leaders = {
+		name: initials.value,
+		time: timerValue,
+	};
+	localStorage.setItem('leaders', JSON.stringify(leaders));
+	renderLeaderboard();
+}
+
+function renderLeaderboard() {
+	// Use JSON.parse() to convert text to JavaScript object
+	var leaders = JSON.parse(localStorage.getItem('leaders'));
+	var place = document.getElementById('leaderboard').children[0];
+	// Check if data is returned, if not exit out of the function
+	console.log(`${leaders.name} --- ${leaders.time}`);
+	console.log(place.children[0].dataset.time);
+	if (leaders !== null) {
+		for (
+			var i = 0;
+			i < 10 && leaders.time > place.children[i].dataset.time;
+			i++
+		) {
+			place.children[
+				i
+			].textContent = `${leaders.name} ----- ${leaders.time} seconds`;
+			place.children[i].setAttribute(`data-time`, `${leaders.time}`);
+			return;
+		}
+	}
+}
+
+// save initials and time to the leaderboard
+saveButton.addEventListener('click', function (event) {
+	event.preventDefault();
+	updateLeaders();
+});
+
+// show the leaderboard on/off
+highScore.addEventListener('click', function () {
+	if (leaderboard.style.display == `block`) {
+		leaderboard.setAttribute(`style`, `display:none;`);
+		resetLeader.setAttribute(`style`, `display:none`);
+	} else {
+		leaderboard.setAttribute(`style`, `display:block;`);
+		resetLeader.setAttribute(`style`, `display:block;`);
+		renderLeaderboard();
+	}
+});
+
+// clear leaderboard
+resetLeader.addEventListener('click', function () {
+	localStorage.clear();
+	for (var i = 0; i < 10; i++) {
+		document.getElementById('leaderboard').children[0].children[
+			i
+		].innerHTML = ``;
+	}
+});
+
+// Reset page for another try at the quiz
+reset.addEventListener('click', function () {
+	window.location.reload();
+});
+
+// Enter key disabled on text for inputs -- from https://stackoverflow.com/users/5105831/tarik
+window.addEventListener(
+	'keydown',
+	function (e) {
+		if (
+			e.keyIdentifier == 'U+000A' ||
+			e.keyIdentifier == 'Enter' ||
+			e.keyCode == 13
+		) {
+			if (e.target.nodeName == 'INPUT' && e.target.type == 'text') {
+				e.preventDefault();
+				updateLeaders();
+				return false;
+			}
+		}
+	},
+	true
+);
